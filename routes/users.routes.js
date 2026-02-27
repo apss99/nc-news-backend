3970556b5e5d71c115ef49ae7cfd7992f5dc117e
April <a.o.hill99@gmail.com>
2026-02-27 15:16:33 +0000

    provide verification for log ins

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -2,11 +2,9 @@ const express = require("express");
 const {
   handleGetAllUsers,
   handleCreateUser,
-  handleGetUserByUsername,
 } = require("../controllers/users.controller");
 
 const router = express.Router();
 router.post("/", handleCreateUser);
 router.get("/", handleGetAllUsers);
-router.get("/:username", handleGetUserByUsername);
 module.exports = router;
