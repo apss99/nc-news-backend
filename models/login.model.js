178e979ab248350574957c38d9e063c8144cde67
April <a.o.hill99@gmail.com>
2026-02-28 15:36:43 +0000

    prevent logging in to deleted users

--- models/login.model.js
+++ models/login.model.js
@@ -2,6 +2,9 @@ const db = require("../db/connection");
 
 exports.fetchUserByUsername = (username) => {
   return db
-    .query("SELECT * FROM users WHERE username = $1", [username])
+    .query(
+      "SELECT * FROM users WHERE username = $1 AND account_deleted = FALSE",
+      [username],
+    )
     .then(({ rows }) => rows[0]);
 };
