8bc1af68fe497c9dd1f15669ee94e55987a589be
April <a.o.hill99@gmail.com>
2026-02-28 00:38:18 +0000

    refactor login functions

--- services/login.service.js
+++ services/login.service.js
@@ -4,8 +4,8 @@ const bcrypt = require("bcrypt");
 exports.makeLoginAttempt = (username, password) => {
   return fetchUserByUsername(username).then((user) => {
     if (!user) throw Error("invalid username");
-    return bcrypt.compare(password, user.password).then((validPassword) => {
-      if (!validPassword) throw new Error("Invalid password");
+    return bcrypt.compare(password, user.password).then((isPasswordValid) => {
+      if (!isPasswordValid) throw new Error("Invalid password");
       return {
         username: user.username,
         avatar_url: user.avatar_url,
